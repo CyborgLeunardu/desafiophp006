da5696f984826dca9b0b91e17a9e7d30514fd74c
Leonardo Castilho <79671553+CyborgLeunardu@users.noreply.github.com>
2024-04-29 21:52:03 -0300

    Update
    Melhorada a formatação do código

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -8,17 +9,12 @@
 </head>
 
 <body>
-<?php 
-        // Caputrando os dados do formulário retroalimentado
-        $dividendo = $_GET['dividendo'] ?? 0;
-        $divisor = $_GET['divisor'] ?? 1;
-     
-    ?>
+
     <main>
         <h1>
             Anatomia de uma Divisão
         </h1>
-        <form action="<?= $_SERVER['PHP_SELF']?>"method="get">
+        <form action="<?= $_SERVER['PHP_SELF'] ?>" method="get">
             <label for="dividendo">Dividendo</label>
             <input type="number" name="dividendo" id="dividendo">
             <label for="divisor">Divisor</label>
@@ -29,13 +25,13 @@
 
     <section id="resultado">
         <h2>Estrutura da Divisão</h2>
-        <?php 
-           $quociente =  intdiv($dividendo,$divisor) ;
-           $resto =  $dividendo % $divisor;
-   
-       
+        <?php
+        $dividendo = $_GET['dividendo'] ?? 0;
+        $divisor = $_GET['divisor'] ?? 1;
+        $quociente =  intdiv($dividendo, $divisor);
+        $resto =  $dividendo % $divisor;
+
         echo "<table class='divisao'>
-  
         <tr>
           <td>$dividendo </td>
           <td>$divisor </td>
@@ -43,11 +39,8 @@
         <tr>
           <td>$resto</td>
           <td>$quociente</td>
-      </table>"
-       ;
-
-
-    
+      </table>";
         ?>
 </body>
+
 </html>
